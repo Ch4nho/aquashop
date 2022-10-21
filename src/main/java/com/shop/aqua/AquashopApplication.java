18e4233dfa42c8448e73fdb54e9cd502054c4ec4
CH KIM <98895025+Ch4nho@users.noreply.github.com>
2022-10-21 21:24:10 +0900

    #0 기초 파일 생성

---
+++ src/main/java/com/shop/aqua/AquashopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.aqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/aqua/AquashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.aqua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
