8c976b8ffd8239f1d4c93479343db05d5d114fed
jiyo12 <j_y1025@naver.com>
2022-11-01 03:47:07 +0900

    테스트

--- src/main/java/com/shop/aqua/controller/HelloController.java
+++ src/main/java/com/shop/aqua/controller/HelloController.java
@@ -1,27 +1,22 @@
 package com.shop.aqua.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
 	@RequestMapping("/")
 	public @ResponseBody String root() throws Exception {
-		return "Hello Test";
+		return "Hello World";
 	}
 	
-	@GetMapping("/test1")
-	public String test1(Model model) {		
+	@RequestMapping("/test1")
+	public String test1(Model model) {
+		model.addAttribute("data", "hello, test1");
 		return "test1";
 	}
-	
-	@GetMapping("/test2")
-	public String test2() {		
-		return "test2";
-	}
-
 }
